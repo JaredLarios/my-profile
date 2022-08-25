19d641cdc035182f273371e170bcf097c7e8508c
JaredLarios <lar21038@byui.edu>
2022-08-24 22:42:38 -0600

    Init App

---
+++ src/Components/Home/style.js
@@ -0,0 +1,23 @@
+import styled, {css} from "styled-components";
+
+export const Header = styled.div`
+
+`;
+
+export const Slider = styled.div`
+
+`;
+
+
+export const Body = styled.div`
+
+`;
+export const Footer = styled.div`
+
+`;
+export const Button = styled.div`
+
+`;
+export const Card = styled.div`
+
+`;
