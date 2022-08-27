d9871848523dc1430e7ab5d34f0c6b008aa5a053
JaredLarios <lar21038@byui.edu>
2022-08-27 09:08:01 -0600

    chore: BYU section

--- src/Components/Home/Body.js
+++ src/Components/Home/Body.js
@@ -18,13 +18,20 @@ const Body = () => {
                         return(
                         <Card
                         name={data.college}
-                        degree={data.deegree}
+                        degree={data.degree}
                         status={data.status}
                         />
                         )
                     }
                 )
             }
+                    
+            <Card 
+            type="university"
+            name={resume.education.bachelor.college}
+            degree={resume.education.bachelor.degree}
+            status={resume.education.bachelor.status}
+            />  
         </Education>
      );
 }
--- src/Components/Home/Card.js
+++ src/Components/Home/Card.js
@@ -39,16 +39,59 @@ const Card = (props) => {
             return ("F")
         }
     }
+
+    const statusOp = (data) =>{
+        if (data !== "university") {
+            return(
+                <ul>
+                    <li>{props.status.current}</li>
+                    <li>{props.status.initDate} - {props.status.graduationDate}</li>
+                    <li>Promedio: {myGp(props.status.gp)} ( {props.status.gp}pts )</li>
+                    <li>{props.status.spotlight}pts</li>
+                </ul>
+            )
+        }
+        else {
+            return(
+                <div>
+                {props.status.map(
+                    (uniData) => {
+                        return(
+                        <div>
+                            <ul>
+                                <li>{uniData.degree}</li>
+                                <li>{uniData.myName}</li>
+                                <li> {uniData.totalClasses}</li>
+                            </ul>
+                            <h3>Clases Aprovadas</h3>
+                            <ul>
+                            {
+                            uniData.passingClasses.map(
+                                (classes) => {return (<li>{classes}</li>)})
+                            }
+                            </ul>
+                            <h3>Clases Actualmente em Curso</h3>
+                            <ul>
+                            {
+                            uniData.currentClasses.map(
+                                (classes) => { return ( <li>{classes}</li>) })
+                            }
+                            </ul>
+                        </div>
+                        )
+                    }
+                )}
+                </div>
+            )
+        }
+    }
     return ( 
         <CardStyle>
-            <h2>{props.name}</h2>
-            <h3>{props.degree}</h3>
-            <ul>
-                <li>{props.status.currentStatus}</li>
-                <li>{props.status.initDate} - {props.status.graduationDate}</li>
-                <li>Promedio: {myGp(props.status.gp)} ( {props.status.gp}pts )</li>
-                <li>{props.status.spotlight}pts</li>
-            </ul>
+            <h3>{props.name}</h3>
+            <h2>{props.degree}</h2>
+            {
+                statusOp(props.type)
+            }
         </CardStyle>
      );
 }
